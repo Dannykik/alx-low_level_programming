b5edf97ec12352674cd81cdcc68cde179065ab18
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-12 05:15:58 -0800

    "TASK 5"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,20 @@
 /**
  * main - Entry point
  *
+ * Make the variables
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int a;
+	char b;
+	double c;
+	float f;
+
+	printf("Size of int is %lu \n", (unsigned long)sizeof(i));
+	printf("Size of char is %lu \n",(unsigned long)sizeof(b));
+	printf("Size of double %lu is \n", (unsigned long)sizeof(c));
+	printf("Size of float %lu is \n", (unsigned long)sizeof(f);
 	return (0);
 }
