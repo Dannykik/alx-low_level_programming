fa6434c0f815bdf764b16657315e59f08f3d6fa6
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-12 03:59:28 -0800

    TASK 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ *
+ * return: return puts fonction 
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
