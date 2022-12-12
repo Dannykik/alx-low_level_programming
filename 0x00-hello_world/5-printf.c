122c5ca6061c1667432346b483d0345947b70501
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-12 03:35:31 -0800

    TASK 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  * return 0
  */
 
-int main(void)
+int main(void) /* main function */   
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0); /* return 0 */
 }
 
 
