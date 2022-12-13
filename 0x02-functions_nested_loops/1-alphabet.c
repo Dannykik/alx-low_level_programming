5a952df40ee5374fa1ec223f984228e12b7e5fdf
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-13 07:00:47 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+		_putchar(alph[i];
+	}
+	_putchar("\n");
+}
