ceee0c7e530a88ff2cc5b0443145308834dfdee3
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-13 08:44:35 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,17 +12,9 @@ int _isalpha(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
 	else if (c >= 'a' && c <= 'z')
         {
                 return (1);
-        }
-        else
-        {
-                return (0);
         }
 	return (0);
 }
