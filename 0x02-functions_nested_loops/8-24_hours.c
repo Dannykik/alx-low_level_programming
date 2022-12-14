c65eab11aaf19263c8ce82962df54583a2163fa3
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-14 12:49:25 -0800

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,32 +4,20 @@
  */
 void jack_bauer(void)
 {
-	int i = 0;
-	int a = 0, b = 0, c = 0, d = 0;
-
-	while (i < 1440)
+	int i, j;
+	i = 0;
+	while (i < 24)
 	{
-		_putchar(a + '0');
-		_putchar(b + '0');
-		_putchar(':');
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar('\n');
-		d++;
-		if (d > 9)
+		j = 0;
+		while (j < 60)
 		{
-			d = 0;
-			c++;
-		}
-		if (c > 5)
-		{
-			c = 0;
-			b++;
-		}
-		if (b > 3)
-		{
-			b = 0;
-			a++;
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
 		i++;
 	}
