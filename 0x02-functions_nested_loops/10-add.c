29cbe9d283b69d0ae9aa0afbf1112cd991793ded
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-14 13:43:20 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a: first value to be added
+ *
+ * @b: second to be added to a
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
