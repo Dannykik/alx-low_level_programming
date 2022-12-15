91692276f5551e6c00a999c45f32f3fb9e161e18
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-15 03:08:55 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - check the code.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i + ' ');
+		i++;
+	}
+	_putchar('\n');
+}
