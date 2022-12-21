2066702ba50e8b19b6b9668e0c9680e825bed6af
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-21 13:12:20 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,26 @@
 /**
  * _strcat - ﹰThe function to concatenate two string
  *
+ * _strlen - the length of character
+ * @s: string to test
+ *
  * @dest: a pointer
  *
  * @src: second pointer
  *
  * Return: always 0
  */
+int _strlen(char *s)                                              {
+	int i;
+
+	int length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
