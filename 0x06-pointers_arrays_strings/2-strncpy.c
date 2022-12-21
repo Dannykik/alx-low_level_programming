b11d16535a6cf1b389664860112643ee4c21e597
Danny Kikwaya <dannykikkikwaya1@gmail.com>
2022-12-21 08:28:08 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,19 @@
  *
  * @n: an integer 
  *
- * Return: Always 0.
+ * Return: Always dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return dest;
+}
