0d403c6d98a940d0dfe8bef913be5d411bfc4d79
Dannykik <dannykikkikwaya1@gmail.com>
2023-01-07 11:11:30 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include  <stdlib.h>
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * @argc: count number that argument contain
+ *
+ * @argv: argument vector*
+ *
+ * Return: Always 0 is succeed
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s \n", argv[0]);
+
+	}
+	return (0);
+}
